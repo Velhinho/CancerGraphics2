93a004b0aaca9999bc51c146497ceead176e145d
Velhinho <goncalo.velhinho420@gmail.com>
2019-10-16 16:10:18 +0100

    All cameras working

--- js/app.js
+++ js/app.js
@@ -32,6 +32,18 @@ function init() {
 
 function update() {
     'use strict';
+
+    update_camera();
+}
+
+function update_camera() {
+    'use strict';
+
+    if(camera_num == 2) {
+        cameras[2].position.x = balls[0].position.x - 10;
+        cameras[2].position.z = balls[0].position.z - 10;
+        cameras[2].lookAt(balls[0].position);
+    }
 }
 
 function animate() {
@@ -172,8 +184,8 @@ function createMovingCamera() {
         window.innerWidth / window.innerHeight,
         1,
         1000);
-    cameras[2].position.x = 0;
+    cameras[2].position.x = balls[0].position.x - 10;
     cameras[2].position.y = 0;
-    cameras[2].position.z = 0;
+    cameras[2].position.z = balls[0].position.z - 10;
     cameras[2].lookAt(balls[0].position);
 }
